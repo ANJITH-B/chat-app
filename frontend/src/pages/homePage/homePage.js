7a6bf6ba56c1f0e94c33cd096ca82d41577f231b
Abijithbabu <abijithb2002@gmail.com>
2023-12-16 14:18:03 +0530

    folder structure updations

---
+++ frontend/src/pages/homePage/homePage.js
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div>
+      <Button>homeP</Button>
+    </div>
+  )
+}
+
+export default homePage;
